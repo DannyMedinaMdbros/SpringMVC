89753d51c20ce3f4753b2fd7268cd00de5eabbed
DannyMedinaMdbros <ADMIN@EQUIPO>
2015-06-29 22:42:59 -0500

    Tree different types of scheduled

--- SpringRestFull/src/main/java/com/springboot/main/batch/GreetingBatchBean.java
+++ SpringRestFull/src/main/java/com/springboot/main/batch/GreetingBatchBean.java
@@ -19,12 +19,46 @@ public class GreetingBatchBean {
 	@Autowired
 	private GreetingService greetingService;
 	
-	@Scheduled(cron = "0.30 * * * * *")
+	//@Scheduled(cron = "0,30 * * * * *")
 	public void cronJob() {
 		logger.info("> clonejob");
 		Collection<Greeting> greetings = greetingService.findAll();
-		logger.info("There are greetings in the data store."+greetings.size());
+		logger.info("There are {} greetings in the data store.",greetings.size());
 		
 		logger.info("< clonejob");
+	}
+	
+	//@Scheduled(initialDelay = 5000, fixedRate = 15000)
+	public void fixedRateJobWithInitialDelay(){
+		logger.info("> fixedRateJobWithInitialDelay");
+		
+		//add scheduled logic here
+		//simulate job processing time
+		long pause = 5000;
+		long start = System.currentTimeMillis();
+		do{
+			if(start + pause < System.currentTimeMillis()){
+				break;
+			}
+		}while(true);
+		logger.info("Processing tiem was {} seconds.", pause/1000);
+		logger.info("< fixedRateJobWithInitialDelay");
+	}
+	
+	@Scheduled(initialDelay = 5000, fixedDelay = 15000)
+	public void fixedDelayJobWithInitialDelay(){
+		logger.info("> fixedRateJobWithInitialDelay");
+		
+		//add scheduled logic here
+		//simulate job processing time
+		long pause = 5000;
+		long start = System.currentTimeMillis();
+		do{
+			if(start + pause < System.currentTimeMillis()){
+				break;
+			}
+		}while(true);
+		logger.info("Processing tiem was {} seconds.", pause/1000);
+		logger.info("< fixedRateJobWithInitialDelay");
 	}
 }
