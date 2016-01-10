d725608306b4d92cfdcad5394476e17d2806ede7
danny <danny@DESKTOP-D610IGM>
2016-01-10 09:17:24 -0600

    factory pattern was created

---
+++ DesignPatterns/src/designe/pattern/factoryPattern/Rectagle.java
@@ -0,0 +1,9 @@
+package designe.pattern.factoryPattern;
+
+public class Rectagle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Inside Rectagle::draw() method.");
+	}
+}
